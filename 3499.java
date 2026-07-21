293be8c1a3d387ab5114faeebab7786c8836f84f
sahil9464-Hub <sharmasahil1806@gmail.com>
2026-07-21 12:06:56 +0530

    Implement maxActiveSectionsAfterTrade method

---
+++ 3499.java
@@ -0,0 +1,26 @@
+lass Solution {
+    public int maxActiveSectionsAfterTrade(String s) {
+        int n = s.length();
+        int ones = 0;
+        int pre = Integer.MIN_VALUE;
+        int maxGain = 0;
+
+        int i = 0;
+        while(i < n){
+            int j = i;
+            while(j < n && s.charAt(j) == s.charAt(i)){
+                j++;
+            }
+            int len = j-i;
+
+            if(s.charAt(i) == '1'){
+                ones += len;
+            }else{
+                maxGain = Math.max(maxGain , pre + len);
+                pre = len;
+            }
+            i = j;
+        }
+        return ones + maxGain;
+    }
+}
