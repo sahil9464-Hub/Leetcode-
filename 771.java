eee3f742806c4f52736cbecbefc549d681208005
sahil9464-Hub <sharmasahil1806@gmail.com>
2026-07-23 19:11:47 +0530

    Create 771.java

---
+++ 771.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int numJewelsInStones(String jewels, String stones) {
+        int count = 0;
+        
+        for(char stone : stones.toCharArray()){
+            for(char jewel : jewels.toCharArray()){
+                if(stone == jewel){
+                    count++;
+                    break;
+
+                }
+            }
+        }
+        return count;
+    }
+}
