7a5097bbebf8b615e43930014aa95fd025b982ff
sahil9464-Hub <sharmasahil1806@gmail.com>
2026-07-29 20:08:21 +0530

    Create 1614.java

---
+++ 1614.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int maxDepth(String s) {
+        int depth = 0;
+        int maxDepth = 0;
+
+        for(int i = 0; i<s.length(); i++){
+            if(s.charAt(i) =='('){
+                depth++;
+                maxDepth = Math.max(maxDepth,depth);
+            }else if(s.charAt(i) == ')'){
+                depth--;
+            }
+        }
+        return maxDepth;
+    }
+}
