6db6ec53fc977b61cefc287e7427d892ff68bcfa
sahil9464-Hub <sharmasahil1806@gmail.com>
2026-08-06 11:10:48 +0530

    Implement method to find the different character

---
+++ 389.java
@@ -0,0 +1,14 @@
+class Solution {
+    public char findTheDifference(String s, String t) {
+        int ans = 0;
+
+        for(char c : s.toCharArray()){
+            ans ^= c;
+        }
+
+        for(char c : t.toCharArray()){
+            ans ^= c;
+        }
+        return (char) ans;
+    }
+}
