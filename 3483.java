3b924341e20c5b6172aa457e413cc9d2fb560a4a
sahil9464-Hub <sharmasahil1806@gmail.com>
2026-09-11 21:07:24 +0530

    Add totalNumbers method to count unique numbers
    Implement method to count unique three-digit numbers from even digits.

---
+++ 3483.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int totalNumbers(int[] digits) {
+        Set<Integer> set = new HashSet<>();
+
+        int n = digits.length;
+
+        for(int i = 0; i < n ; i++){
+            if(digits[i] % 2 != 0){
+                continue;
+            }
+            for(int j = 0; j < n ; j++){
+                if(j == i){
+                    continue;
+                }
+                for(int k = 0; k < n ; k++){
+                    if(k == i || k == j || digits[k] == 0){
+                        continue;
+                    }
+
+                    int num = digits[k] * 100 + digits[j] * 10 + digits[i];
+
+                    set.add(num);
+                }   
+            }
+        }
+        return set.size();
+    }
+}
